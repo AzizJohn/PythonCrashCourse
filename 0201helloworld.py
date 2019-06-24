3ed0f54cc903ddb38c5eaf944cc0616b54a0a995
Cosy Cagesong <a10063368@gmail.com>
2019-06-25 06:39:25 +0800

    Chapter 02

---
+++ 0201helloworld.py
@@ -0,0 +1,16 @@
+string = "HeLLo woRld!"
+print(string.title())
+print(string.upper())
+integer = 18
+flt = 3.141
+format_string = f"a{string}b{integer}c{flt}"
+print(format_string)
+rm_white_space = "  python   "
+print(rm_white_space.rstrip())
+print(rm_white_space.lstrip())
+print(4/1)# =4.0
+print(4//1)# =4
+universe_age = 14_3_1_1_000000
+print(universe_age)
+x, y, z = 0, 0, 0
+import this
