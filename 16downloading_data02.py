504c099c214c007413958c9e121b8b1d78843500
Cosy Cagesong <a10063368@gmail.com>
2019-07-09 15:48:36 +0800

    Chapter 16 added comments to 16-02.py

--- 16downloading_data02.py
+++ 16downloading_data02.py
@@ -1,8 +1,12 @@
 """make a readable json file readable by adding indentation."""
 import json
 filename = '16data/eq_data_1_day_m1.json'
-with open(filename) as f:
-	all_eq_data = json.load(f)
+with open(filename) as f:#f is a file object.
+	all_eq_data = json.load(f)#all_eq_data is a json object.
 reader = '16data/reader.json'
 with open(reader, 'w') as f:
 	json.dump(all_eq_data, f, indent=4)
+#The json.dump() function takes a JSON data object and a file object, and
+#writes the data to that file
+#The indent=4 argument tells dump() to format the data using indentation
+#that matches the data’s structure.
